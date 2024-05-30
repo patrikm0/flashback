905f795cd4e34c53fd5d83466bab00142d8732f4
Jacin Ali <jacinali172@gmail.com>
2024-05-30 11:36:37 +0200

    some games added

---
+++ games.js
@@ -0,0 +1,84 @@
+const game = {
+    
+    "Fireboy and Watergirl 1": {
+        Title: "Fireboy and Watergirl 1: The Forest Temple",
+        Released: "2009-11-19",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Oslo Albet",
+        Plot: "Fireboy and Watergirl, are exploring the Forest Temple in search of diamonds. Switch between Fireboy & Watergirl and play alone or together as 2 players. The goal is to reach the exit safely, so be careful. Fireboy cannot touch water and Watergirl cannot touch fire. ",
+        Poster: "https://img.gamedistribution.com/a55c9cc9c21e4fc683c8c6857f3d0c75-512x340.jpeg",
+        Iframe: "https://html5.gamedistribution.com/a55c9cc9c21e4fc683c8c6857f3d0c75/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+
+    "Fireboy and Watergirl 2": {
+        Title: "Fireboy and Watergirl 2: The Light Temple",
+        Released: "2010-09-28",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Oslo Albet",
+        Plot: "This time, Fireboy and Watergirl, are exploring the Light Temple. Use the temple light to reach the exit safely. Switch between Fireboy & Watergirl, but be careful, Fireboy cannot touch water and Watergirl cannot touch fire.",
+        Poster: "https://img.gamedistribution.com/383ad09b92c7446b9113cccc29630517-512x384.jpeg",
+        Iframe: "https://html5.gamedistribution.com/383ad09b92c7446b9113cccc29630517/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+
+    "Fireboy and Watergirl 3": {
+        Title: "Fireboy and Watergirl 3: The Ice Temple",
+        Released: "2012-03-21",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Oslo Albet",
+        Plot: "Fireboy and Watergirl are having a very 'cool' time on their journey in the Ice Temple! A lot of dangers are lurking inside the ice temple. You must complete many challenges to get these brave heroes to the exit safely. Switch between Fireboy & Watergirl- but watch out; Fireboy cannot touch water, and Watergirl cannot touch fire.",
+        Poster: "https://img.gamedistribution.com/f3a6e1ac0a77412289cbac47658b2b68-512x384.jpeg",
+        Iframe: "https://html5.gamedistribution.com/f3a6e1ac0a77412289cbac47658b2b68/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+
+    "Fireboy and Watergirl 4": {
+        Title: "Fireboy and Watergirl 4: The Crystal Temple",
+        Released: "2013-03-01",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Oslo Albet",
+        Plot: 	"Fireboy and Watergirl arrived in their journey to the crystal temple. The new Fireboy and Wategirl adventure requires them to work together to solve each level. This game has a new twist, the players can jump from one place to another using the portals. Pay attention to the portals color, a black portal links to another black portal and a white portal links white portal. Don't forget to collect all the diamonds in each level the red, blue and the gray.",
+        Poster: "https://img.gamedistribution.com/3790681b69584409b7f681a8e400102d-512x384.jpeg",
+        Iframe: "https://html5.gamedistribution.com/3790681b69584409b7f681a8e400102d/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+    
+    "Fireboy and Watergirl 5": {
+        Title: "Fireboy and Watergirl 5: Elements",
+        Released: "2019-01-14",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Oslo Albet",
+        Plot: 	"Explore the Elemental Temples in Fireboy and Watergirl 5 (by Oslo Albet)! Solve many puzzles, collect all the diamonds and beat the time. Fireboy and Watergirl Elements is a completely new game with lots of new elements. Travel to all temples in the Fireboy Watergirl series. Enter the new Fire Temple, The Forest Temple, The Ice Temple, The Light Temple, The Wind Temple and The Crystal Temple. In FBWG5 you can play the adventure alone or with a friend. Reach the exit safely.",
+        Poster: "https://img.gamedistribution.com/03f124074b0049f89fdace0cacf8ead5-512x384.jpeg",
+        Iframe: "https://html5.gamedistribution.com/03f124074b0049f89fdace0cacf8ead5/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+    
+    "Fireboy and Watergirl 6": {
+        Title: "Fireboy and Watergirl 6: Fairy Tales",
+        Released: "2021-11-01",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Oslo Albet",
+        Plot: 	"Fireboy and Watergirl need help to explore this new temple. This time Fairies will help them solve the puzzles, beat the times and collect all the diamonds! Play alone or with a friend to join Fireboy and Watergirl in this new adventure! Turn levers and push buttons to complete these 30 new levels of the official Fireboy and Watergirl series.",
+        Poster: "https://img.gamedistribution.com/56da8a54fe204845b34ccff750d4a60b-512x384.jpeg",
+        Iframe: "https://html5.gamedistribution.com/56da8a54fe204845b34ccff750d4a60b/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+    
+    "Jetpack Joyride": {
+        Title: "Jetpack Joyride",
+        Released: "2011-09-01",
+        Genres: ["Koop", "Puzzle"],
+        Publisher: "Halfbrick Studios",
+        Plot: 	"Bullet-powered jetpacks! Giant mechanical dragons! Birds that shoot money! From the makers of Fruit Ninja comes this insane, high-flying endless runner that will keep you on the edge of your seat! It's time to join Barry Steakfries and equip cool jetpacks, suit up in stylish costumes, and ride crazy vehicles in his endless running quest to beat the scientists to the end of the lab.",
+        Poster: "https://img.gamedistribution.com/c1d742d5feda42d981ff07eab0c809de-512x384.jpg",
+        Iframe: "https://html5.gamedistribution.com/c1d742d5feda42d981ff07eab0c809de/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}"
+    },
+
+    "Slope": {
+        Title: "Slope",
+        Released: "2014-09-30",
+        Genres: ["Single Player", "Ball", "Jump and Run", "Obstacle"],
+        Publisher: "Y8 Studios",
+        Plot: 	"Slope is the ultimate running game that will put your skills to the test. Speed down on a randomized slope. The farther you go, the faster your ball travels. This game might look simple but playing this will give you extreme adrenaline rush. Just remember to avoid obstacles and those red blocks. Always be on track to get a high score and you might have your name on the leaderboard!",
+        Poster: "https://play-lh.googleusercontent.com/FY9FXA14xl5ExZDiLqxEQRUFu8Lau0gM37zMRK5yFk_cadt_a3i5romTW-kbFlk-RQI=w526-h296-rw",
+        Iframe: "https://www.y8.com/embed/slope"
+    }
+};
+
+module.exports = game; 
